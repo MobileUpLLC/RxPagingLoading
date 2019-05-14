289d348806e3e196968de5cf4e60a86472d06df6
Dmitriy Gorbunov <dg@mobileup.ru>
2019-05-14 17:43:51 +0300

    Additional test for LcePm

--- rxplce/rxplce/src/test/kotlin/ru/mobileup/rxplce/LcePmImplTest.kt
+++ rxplce/rxplce/src/test/kotlin/ru/mobileup/rxplce/LcePmImplTest.kt
@@ -5,14 +5,20 @@ import io.reactivex.Completable
 import io.reactivex.Single
 import me.dmdev.rxpm.PresentationModel.Lifecycle.CREATED
 import me.dmdev.rxpm.test.PmTestHelper
+import org.junit.Rule
 import org.junit.Test
+import ru.mobileup.rxplce.util.SchedulersRule
 import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 class LcePmImplTest {
 
     private val loadingDataSource = Single.just("foo")
     private val error = IOException()
     private val loadingErrorSource = Single.error<String>(error)
+
+    @get:Rule
+    val schedulers = SchedulersRule(true)
 
     @Test fun initialState() {
 
@@ -214,6 +220,59 @@ class LcePmImplTest {
 
             LcePm.DataState(
                 data = "bar",
+                refreshingError = null,
+                refreshing = false
+            )
+        )
+    }
+
+    @Test fun blockRepeatedRefreshing() {
+
+        val relay = BehaviorRelay.create<String>()
+        val lcePm = LcePmImpl(
+            refreshData = Single.just("foo")
+                .delay(1, TimeUnit.SECONDS)
+                .doOnSuccess {
+                    relay.accept("foo")
+                }
+                .ignoreElement(),
+            dataChanges = relay
+        )
+
+        val pmTestHelper = PmTestHelper(lcePm)
+
+        pmTestHelper.setLifecycleTo(CREATED)
+
+        val testObserver = lcePm.dataState.observable.test()
+
+        lcePm.refreshes.consumer.accept(Unit)
+        lcePm.refreshes.consumer.accept(Unit)
+        lcePm.refreshes.consumer.accept(Unit)
+
+        schedulers.testScheduler.advanceTimeTo(3, TimeUnit.SECONDS)
+
+        testObserver.assertValues(
+
+            LcePm.DataState(
+                data = null,
+                refreshingError = null,
+                refreshing = false
+            ),
+
+            LcePm.DataState(
+                data = null,
+                refreshingError = null,
+                refreshing = true
+            ),
+
+            LcePm.DataState(
+                data = "foo",
+                refreshingError = null,
+                refreshing = true
+            ),
+
+            LcePm.DataState(
+                data = "foo",
                 refreshingError = null,
                 refreshing = false
             )
